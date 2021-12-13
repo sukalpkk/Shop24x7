3ffafb88b4f175aa0ec3eb0a4c13e913b1130227
Sukalp <sukalp.kokse001@gmail.com>
2021-12-13 18:43:10 -0500

    Admin - crud operation on user

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminAddUserComponent } from './components/admin-add-user/admin-add-user.component';
+import { AdminEditUserComponent } from './components/admin-edit-user/admin-edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminAddUserComponent,
+    AdminEditUserComponent
   ],
   imports: [
     BrowserModule,
