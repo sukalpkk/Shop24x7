5daf5c4ba0e581399d97d3ed27e1481304fd2e06
Sukalp <sukalp.kokse001@gmail.com>
2021-12-14 12:31:22 -0500

    New Features

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminAddUserComponent } from './components/admin-add-user/admin-add-user.component';
 import { AdminEditUserComponent } from './components/admin-edit-user/admin-edit-user.component';
+import { AdminAddNewProductComponent } from './components/admin-add-new-product/admin-add-new-product.component';
+import { AdminManageProductComponent } from './components/admin-manage-product/admin-manage-product.component';
+import { AdminManageOrderComponent } from './components/admin-manage-order/admin-manage-order.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductPriceDirective } from './directives/product-price.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminAddUserComponent,
-    AdminEditUserComponent
+    AdminEditUserComponent,
+    AdminAddNewProductComponent,
+    AdminManageProductComponent,
+    AdminManageOrderComponent,
+    ProductPriceDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
