c646b17c896f05b9a1631e3d08d466b34a2e1d98
Sukalp <sukalp.kokse001@gmail.com>
2021-12-14 17:48:18 -0500

    Newly Added CSS and Features

--- frontend/src/app/components/admin-manage-product/admin-manage-product.component.ts
+++ frontend/src/app/components/admin-manage-product/admin-manage-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveMustMatch } from 'src/app/directives/reactive-must-match.validator';
 
 @Component({
   selector: 'app-admin-manage-product',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminManageProductComponent implements OnInit {
 
-  constructor() { }
+  public adminAddNewProductForm:FormGroup;
+  public isadminAddNewProductFormSubmitted:boolean;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { 
+    this.adminAddNewProductForm = {} as FormGroup;
+    this.isadminAddNewProductFormSubmitted = false;
+
   }
 
+  public ngOnInit(): void {
+    this.initialize();
+  }
+
+  public initialize(){
+    this.adminAddNewProductForm = this.formBuilder.group({
+      productname:['',[Validators.required,Validators.maxLength(64)]],
+      department:['',[Validators.required]],
+      price:['',[Validators.required]],
+      discountprice:['',[Validators.required]],
+      productimage:['',[Validators.required]],
+      productdescription:['',[Validators.required,Validators.minLength(20)]],
+    },{
+      validator: ReactiveMustMatch('price','discountprice')
+    })
+  }
+
+  get adminFormControls(){
+    return this.adminAddNewProductForm.controls;
+  }
+
+  public onSubmit():void{
+    this.isadminAddNewProductFormSubmitted = true;
+    
+  }
 }
