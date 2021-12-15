7ef68bcf7a214d896de496f52c29ddfcf966ad1c
Sukalp <sukalp.kokse001@gmail.com>
2021-12-15 18:33:41 -0500

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -13,27 +13,27 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/product',productRoute)
 
-// app.get('/', (req, res) => {
-//     profileModel.find((err,result)=>{
-//         if (err) { throw err; }
-//         else{
-//             res.render('index.ejs', {profiles: result})
-//         }
-//     })
+app.get('/', (req, res) => {
+    profileModel.find((err,result)=>{
+        if (err) { throw err; }
+        else{
+            res.render('index.ejs', {profiles: result})
+        }
+    })
   
-// })
+})
 
 
-// app.post('/addProfile', (req, res) => {
-//     profileModel.create(req.body, (err, results) => {
-//         if (err) {
-//             return console.log(err);
-//         } else {
-//             console.log('saved to database')
-//             res.redirect('/')
-//         }
-//     })
-// })
+app.post('/addProfile', (req, res) => {
+    profileModel.create(req.body, (err, results) => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log('saved to database')
+            res.redirect('/')
+        }
+    })
+})
 
 app.listen(8080, function () {
     console.log("server started in Port 8080")
