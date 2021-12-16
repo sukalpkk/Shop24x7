c6989fb3699140274b5aab1d88586db5b15ab271
SamhithReddy <63306266+SamhithReddy@users.noreply.github.com>
2021-12-16 17:53:48 -0500

    Update AuthController.js

--- backend/auth/AuthController.js
+++ backend/auth/AuthController.js
@@ -17,38 +17,50 @@ const session = require('express-session');
 
 
 router.use(session({
-    secret: 'edurekaSecert1',
-    resave: false,
-    saveUninitialized: true
-  }));
+  secret: 'edurekaSecert1',
+  resave: false,
+  saveUninitialized: true
+}));
 
 
-  router.post('/register',function(req,res){
-    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
-     
-    User.create({
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        email: req.body.email,
-        password: hashedPassword
-    }, function (err,user){
-        if (err){
-            return res.status(500).send("There was a problem registering the user.")
-        } else{
-            // const token =  jwt.sign({
-            //     id: user._id
-            //   }, config.secret, {
-            //     expiresIn: 86400 // expires in 24 hours
-            //   });
-             res.send({status:"success", message:"user created successfully"})
-        }
+router.use(bodyParser.urlencoded({
+  extended: false
+}));
+router.use(bodyParser.json());
 
-    })
+
+router.post('/register', function (req, res) {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+  User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: hashedPassword,
+    confirmPassword: hashedPassword,
+    profileImage: '',// creating a dummy image
+    address: { /// creating a empty address
+      type: Object,
+      default: {}
+    }
+  }, function (err, user) {
+    if (err) {
+      return res.status(500).send("There was a problem registering the user.")
+    } else {
+      const token = jwt.sign({
+        id: user._id
+      }, config.secret, {
+        expiresIn: 86400 // expires in 24 hours
+      });
+      res.send({ status: "success", message: "user created successfully" })
+    }
+
+  })
   // Login User
-  router.post('/login', function(req, res) {
+  router.post('/login', function (req, res) {
     User.findOne({
       email: req.body.email
-    }, function(err, user) {
+    }, function (err, user) {
       if (err) return res.status(500).send('Error on the server.');
       const string = encodeURIComponent('! Please enter valid value');
       if (!user) {
@@ -56,45 +68,51 @@ router.use(session({
       } else {
         const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
         if (!passwordIsValid) return res.status(401).send({
-            auth: false,
-            token: null
-          });
+          auth: false,
+          token: null
+        });
         var token = jwt.sign({
           id: user._id
         }, config.secret, {
           expiresIn: 86400 // expires in 24 hours
         });
         localStorage.setItem('authtoken', token)
-        return res.send({status:"success", message:"user logged in successfully",accessToken:token})
+        return res.send({
+          status: "success",
+          message: "user logged in successfully",
+          accessToken: token,
+          email: user.email,
+          userrole: user.userrole
+        })
       }
     });
   });
-  })
+})
 
-  // Info of logined User
-router.get('/loginedUser', function(req, res) {
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({
-        auth: false,
-        message: 'No token provided.'
-      });
-  
-    jwt.verify(token, config.secret, function(err, decoded) {
-      if (err) return res.status(500).send({
-          auth: false,
-          message: 'Failed to authenticate token.'
-        });
-  
-      // res.status(200).send(decoded);
-      User.findById(decoded.id, {
-        password: 0
-      }, function(err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-  
-        res.status(200).send(user);
-      });
-    });
+// Info of logined User
+router.get('/loginedUser', function (req, res) {
+  var token = req.headers['x-access-token'];
+  if (!token) return res.status(401).send({
+    auth: false,
+    message: 'No token provided.'
   });
 
-  module.exports = router;
+  jwt.verify(token, config.secret, function (err, decoded) {
+    if (err) return res.status(500).send({
+      auth: false,
+      message: 'Failed to authenticate token.'
+    });
+
+    // res.status(200).send(decoded);
+    User.findById(decoded.id, {
+      password: 0
+    }, function (err, user) {
+      if (err) return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+
+      res.status(200).send(user);
+    });
+  });
+});
+
+module.exports = router;
