a7319ca4060c53dcd6db3a48fd23b41a0ba119be
Logan Larson <2121772@TCS.com>
2021-12-16 10:47:14 -0700

    Propper backend

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ const app = express();
 const db = require('./db');
 const cors = require('cors');
 app.use(cors())
-const ProfileController = require('./profile/ProfileController');
-app.use('/users', ProfileController);
+const UserController = require('./user/UserController');
+app.use('/users', UserController);
 
 const AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
