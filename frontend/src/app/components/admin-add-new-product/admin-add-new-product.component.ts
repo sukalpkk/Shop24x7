2065270bcb447928deeb25470b4ef7b47efedf7c
Sukalp <sukalp.kokse001@gmail.com>
2021-12-16 13:19:58 -0500

    product UI fixed

--- frontend/src/app/components/admin-add-new-product/admin-add-new-product.component.ts
+++ frontend/src/app/components/admin-add-new-product/admin-add-new-product.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-<<<<<<< HEAD
 import { ProductPrice } from 'src/app/directives/product-price.validator';
 import { ReactiveMustMatch } from 'src/app/directives/reactive-must-match.validator';
 import { AddNewProductService } from 'src/app/services/add-new-product.service';
@@ -8,12 +7,6 @@ import { first } from 'rxjs/operators';
 import { AlertService } from 'src/app/services/alert.service';
 import { Router } from '@angular/router';
 
-=======
-import { Router } from '@angular/router';
-import { ProductPrice } from 'src/app/directives/product-price.validator';
-import { ReactiveMustMatch } from 'src/app/directives/reactive-must-match.validator';
-import { AdminAddNewProductService } from 'src/app/services/admin-add-new-product.service';
->>>>>>> main
 @Component({
   selector: 'app-admin-add-new-product',
   templateUrl: './admin-add-new-product.component.html',
@@ -23,7 +16,6 @@ export class AdminAddNewProductComponent implements OnInit {
 
   public adminAddNewProductForm:FormGroup;
   public isadminAddNewProductFormSubmitted:boolean;
-<<<<<<< HEAD
   public addProductDetail: any;
 
   constructor(private formBuilder: FormBuilder, private addProductService:AddNewProductService,private alertService: AlertService,private router: Router,
@@ -31,14 +23,6 @@ export class AdminAddNewProductComponent implements OnInit {
     this.adminAddNewProductForm = {} as FormGroup;
     this.isadminAddNewProductFormSubmitted = false;
     this.addProductDetail={};
-=======
-  public productDetails:any;
-
-  constructor(private formBuilder: FormBuilder,private adminAddNewProduct: AdminAddNewProductService,private router:Router) { 
-    this.adminAddNewProductForm = {} as FormGroup;
-    this.isadminAddNewProductFormSubmitted = false;
-    this.productDetails = {}
->>>>>>> main
 
   }
 
@@ -65,7 +49,6 @@ export class AdminAddNewProductComponent implements OnInit {
 
   public onSubmit():void{
     this.isadminAddNewProductFormSubmitted = true;
-<<<<<<< HEAD
 
     if (this.adminAddNewProductForm.invalid) {
       return;
@@ -88,18 +71,4 @@ export class AdminAddNewProductComponent implements OnInit {
   }
 
   }
-=======
-    if(this.adminAddNewProductForm.valid){
-      this.productDetails = this.adminAddNewProductForm.getRawValue();
-      this.adminAddNewProduct.addNewProduct(this.productDetails).subscribe((response):any=>{
-        this.adminAddNewProductForm.reset();
-      })
-    }else{
-      alert("Enter all Fields")
-    }
-    
-  }
-
-
->>>>>>> main
 }
