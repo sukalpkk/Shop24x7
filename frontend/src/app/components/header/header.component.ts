2b61fbd288dcf7c839c5b3a757e4fc91f3bbfb37
SamhithReddy <63306266+SamhithReddy@users.noreply.github.com>
2021-12-16 23:36:52 -0500

    Add files via upload

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthGuard } from 'src/app/components/helpers/auth.gaurd';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isAdmin:boolean=false;
+  loginStatus:boolean = false;
+  constructor(private _authGuard:AuthGuard) { }
 
   ngOnInit(): void {
+    this.loginStatus = this._authGuard.isLoggedIn();
+    this.isAdmin = this._authGuard.isAdmin();
+
+    console.log("logged in: " + this.loginStatus);
+    console.log('Admin or normal user:'+ this.isAdmin);
+  
+  }
+  
+  title = 'frontend';
+
+  logout(){
+    this._authGuard.logout();
+    
   }
 
 }
