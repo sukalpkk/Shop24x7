25182c98fbab59072642929b6a45f0351fcc4b25
SamhithReddy <63306266+SamhithReddy@users.noreply.github.com>
2021-12-16 23:25:41 -0500

    Add files via upload

--- frontend/src/app/models/productdetails.model.ts
+++ frontend/src/app/models/productdetails.model.ts
@@ -23,3 +23,17 @@ export class ProductDetails implements ProductDetails{
     created_on!:String;
     isTopProduct!:String;
 }
+export class Product{
+    id!:number;
+    productName!:String;
+    category!:String;
+    department!:String;
+    price!:number;
+    discountPrice!:number;
+    description!:String;
+    image!:String;
+    created_on!:String;
+    topSelling!:Boolean;
+    isTopProduct!:String;
+}
+
