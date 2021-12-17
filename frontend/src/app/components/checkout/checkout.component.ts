609266658b4a1886b6c22ead34aaa45b9a60c2b8
Logan Larson <2121772@TCS.com>
2021-12-16 21:09:24 -0700

    checkout functional

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -15,19 +15,15 @@ import { OrderService } from 'src/app/services/order.service';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-  public shippingAddressForm : FormGroup ;
   public checkoutForm : FormGroup ;
   public userAddress:any;
-  public isUserAddressFormSubmitted:boolean;
   public isUserAddressEditable:boolean;
   public isCheckoutFormSubmitted:boolean;
   public user:IUser;
 
   constructor(private formBuilder: FormBuilder, private orderService:OrderService, private httpClient:HttpClient, private cartService:CartService, private router:Router, private authenticationService: AuthenticationService, private userService:UserService) { 
-    this.shippingAddressForm = {} as FormGroup;
     this.checkoutForm = {} as FormGroup;
     this.userAddress = {}
-    this.isUserAddressFormSubmitted = false;
     this.isUserAddressEditable = false;
     this.isCheckoutFormSubmitted = false;
     this.user = {} as IUser;
@@ -35,34 +31,37 @@ export class CheckoutComponent implements OnInit {
   }
 
   public ngOnInit(): void {
+    this.initializeCheckoutForm();
+    
     this.user=this.authenticationService.currentUserValue;
 
     if (this.user != {} as IUser){
 
       
-    //   this.userService.getUserById(this.id).subscribe(result=>{
+      this.userService.getProfile("this.user.email").subscribe((result:IUser)=>{
 
-    //   this.user=result;
-    // },error=>{
-    //   console.log(error);
-    // })
+      this.user=result;
+      
+    },error=>{
+      console.log(error);
+
+    })
   } else {
     alert('Please log in.');
     this.router.navigate(['/login']);
   }
 
-    this.initializeCheckoutForm();
   }
 
   public initializeCheckoutForm(){
     this.checkoutForm = this.formBuilder.group({
-      firstname:['',[Validators.required]],
-      lastname:['',[Validators.required]],
-      email:['',[Validators.required]],
-      streetAdress:['',[Validators.required]],
+      firstname:[this.user.firstname||'',[Validators.required]],
+      lastname:[this.user.lastname||'',[Validators.required]],
+      email:[this.user.email||'',[Validators.required,Validators.email]],
+      streetAddress:['',[Validators.required]],
       city:['',[Validators.required]],
-      state:['',[Validators.required,Validators.email]],
-      zip:['',[Validators.required]],
+      state:['',[Validators.required]],
+      zipCode:['',[Validators.required]],
     })
   }
 
@@ -71,36 +70,45 @@ export class CheckoutComponent implements OnInit {
   }
 
   public onPlaceOrder():void{
-    this.userService.updateProfile(this.user).subscribe(result=>{
-      console.log(result)
-    }, error=>{
-      console.log(error)
-    })
-
-  
-    let items:ProductDetails[] = this.cartService.getItems();
-    let total = this.cartService.getTotal();
-    let stringifiedCart:any = [];
-    items.forEach((item) => {
-      stringifiedCart.push(item.name);
-    });
-
-
-    let order:any = {"userEmail":this.authenticationService.currentUser, "items":stringifiedCart, "total":total}
-
-    sessionStorage.removeItem('cart');
-    this.cartService.clearCart();
-  
-    this.orderService.postOrders(order).subscribe(result=>{
-      alert(result);
-      this.router.navigate(['/orders']);
-
-    },(err)=>{console.log(err) })
-
-    this.isUserAddressFormSubmitted = true;
-    this.userAddress = this.shippingAddressForm.getRawValue();
+    this.isCheckoutFormSubmitted = true;
+    this.user = this.checkoutForm.getRawValue();
     //this.profile.address = this.shippingAddressForm.getRawValue();
     this.isUserAddressEditable = false;
+
+
+    if(this.checkoutForm.valid){
+
+      this.userService.updateProfile(this.user).subscribe(result=>{
+        console.log(result)
+      }, error=>{
+        console.log(error)
+      })
+
+    
+      let items:ProductDetails[] = this.cartService.getItems();
+      let total = this.cartService.getTotal();
+      let stringifiedCart:any = [];
+      items.forEach((item) => {
+        stringifiedCart.push(item.name);
+      });
+
+
+      let order:any = {"userEmail":this.authenticationService.currentUser, "items":stringifiedCart, "total":total}
+
+      sessionStorage.removeItem('cart');
+    
+      console.log("OOORRRDDERR")
+
+      this.orderService.postOrders(order).subscribe(result=>{
+        alert(result);
+        this.cartService.clearCart();
+        this.router.navigate(['/order']);
+
+      },(err)=>{console.log(err)
+        this.router.navigate(['/order']);
+       })
+
+    }
     // this.userformService.postReactiveUsers(this.userProfile).subscribe((response):any=>{
     //   console.log(response,"reactive form")
     // })
